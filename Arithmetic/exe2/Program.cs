cea1e72977b720ce8a62a06706ef06cc2acbd53b
Humigan <82048732+Humigan@users.noreply.github.com>
2021-07-13 04:08:06 +0300

    Exercises

---
+++ Arithmetic/exe2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            for (int i = 0; i < 999; i++)
+            {
+                Console.WriteLine("Enter number to check it is even or odd");
+                a = int.Parse(Console.ReadLine());
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("Even Number");
+                }if (a % 2 == 1) 
+                {
+                    Console.WriteLine("Odd Number");
+                }
+
+                Console.WriteLine("Continue?");
+                Console.WriteLine("1 - yes / 2 - no");
+                b = int.Parse(Console.ReadLine());
+                if (b == 1)
+                {
+                    continue;
+                }if (b == 2) 
+                {
+                    break;
+                }
+                if (b != 1 || b != 2)
+                {
+                    Console.WriteLine("ERROR");
+                    break;
+                }
+            }
+            Console.WriteLine("bye!");
+        }
+    }
+}
---
+++ Arithmetic/exe3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exee3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int sum = 0;
+            int count = 0;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            for (int i = a; i < b+1; i++)
+            {
+                sum = sum + i;
+                count++;
+            }
+            Console.WriteLine(sum);
+
+            Console.WriteLine(sum/count);
+        }
+    }
+}
