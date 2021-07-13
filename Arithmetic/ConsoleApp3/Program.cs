36314dc58d81d264e8024f7828edd23fbb882c76
Humigan <82048732+Humigan@users.noreply.github.com>
2021-07-13 03:59:33 +0300

    exe 1-6

---
+++ Arithmetic/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+           a = int.Parse(Console.ReadLine());
+           b = int.Parse(Console.ReadLine());
+            bool Either = false;
+
+            if (a == 15 || b == 15 || a+b == 15 || a-b == 15 || b-a == 15)
+            {
+                Either = true;
+            }
+            Console.WriteLine(Either);
+        }
+    }
+}
